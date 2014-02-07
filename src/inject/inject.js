14ad39455d7ee432f3d84110e76cffe3b407977d
Sai Pranav <saipranav@LT-PC-546U.(none)>
2014-02-07 12:40:36 +0530

    inital commit

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,7 +1,25 @@
+var inputFields = new Array();
+
 //Jquery function to insert master password fields in the web page (DOM modifications).
 //It sees for the input type password and inserts the new text box (master password) before.
+//On hover of password fields it shows the password fields on 2 second gap.
 $("input[type='password']").each(function(){
 	$(this).before('<input type="password" id="master_password" placeholder="Master Password"></input>');
+	inputFields.push($(this));
+	$(this).mouseenter(function(){
+		inputFields[inputFields.length-1].prop('type','text');
+	});
+	$(this).mouseleave(function(){
+		inputFields[inputFields.length-1].prop('type','password');
+	});
+});
+
+//show master password on hover
+$("input#master_password").mouseenter(function(){
+	$(this).prop('type','text');
+});
+$("input#master_password").mouseleave(function(){
+	$(this).prop('type','password');
 });
 
 //Single message handler function to handle messages from content scripts.
