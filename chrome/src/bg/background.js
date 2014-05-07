61c16bca538db4ececab6a742fda6e62556c463c
Sai Pranav <rsaipranav92@gmail.com>
2014-05-07 08:54:09 +0530

    fixed current tab problem, browser action broadcasts messages to all tabs in safari, chrome, firefox
    safari browser action reacts same as in firefox and chrome

--- chrome/src/bg/background.js
+++ chrome/src/bg/background.js
@@ -1,8 +1,7 @@
 //Global variable site tag (eg: google).
 var siteTag = "";
-
-//Global variable tab (current tab).
-var currentTab = undefined;
+//Global variable for web page url. Useful for performance in findSiteTag.
+var siteUrl = "";
 
 //For web pages containing login form dynamically generated through ajax.
 //This function listens to xmlhttprequests and reruns the DOM modification script in content script.
@@ -10,7 +9,7 @@ chrome.webRequest.onCompleted.addListener(function(info){
 		/*chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 			chrome.tabs.sendMessage(tabs[0].id,{result:"rerun input script"});
 		});*/
-		chrome.tabs.sendMessage(currentTab.id,{result:"rerun input script"});
+		chrome.tabs.sendMessage(info.tabId,{result:"rerun input script"});
 	},
 	{
 		urls: ["<all_urls>"],
@@ -22,62 +21,68 @@ chrome.webRequest.onCompleted.addListener(function(info){
 //Called for every keyup in master password field.
 //Returns the password from algorithm to content script.
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	chrome.tabs.sendMessage(currentTab.id,{ result: Hasher.passy(request.masterPassword,siteTag), fromInputField: request.fromInputField });
+	findSiteTag(sender.url);
+	chrome.tabs.sendMessage(sender.tab.id,{ result: Hasher.passy(request.masterPassword,siteTag), fromInputField: request.fromInputField });
 });
 
-//For every update in tab (eg: opening google) this will capture the siteTag.
-//New tab is set as global variable.
-//TODO: some best approach if possible.
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if(changeInfo.status == "complete"){
-		siteTag = "";
-
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-			currentTab = tabs[0];
-			//Split the url with ".".
-			var siteUrlBreakupArray = tabs[0].url.split(".");
-
-			if(siteUrlBreakupArray.length == 0){
-				siteTag = siteUrlBreakupArray[0];
-			}
-
-			//check the array strings with the pattern.
-			for(var i=0; i<siteUrlBreakupArray.length; i++){
-				var tempVar = siteUrlBreakupArray[i];
-				if(tempVar.match("^com") || tempVar.match("^org") || tempVar.match("^gov")){
-					siteTag = siteUrlBreakupArray[i-1];
-					break;
-				}
-				else if(tempVar.match("^co")){
-					siteTag = siteUrlBreakupArray[i-1];
-					break;
-				}
-				else if(tempVar.match("^in")){
-					siteTag = siteUrlBreakupArray[i-1];
-					break;
-				}
-			}
-
-			//Remove the http:// tag if its in siteTag there.
-			if(siteTag.indexOf("://") > 0){
-				siteTag = siteTag.substr( siteTag.indexOf("://")+3, siteTag.length );
-			}
-			window.alert(siteTag);
-		});
-	}
-});
-
+//Toggle browser actions.
 chrome.browserAction.onClicked.addListener(function togglePasswordDiv(){
 	chrome.browserAction.getTitle({},function(title){
 		if(title == "Click to disable extrasafe"){
-			chrome.tabs.sendMessage(currentTab.id,{result:"disable password div"});
+			broadcast({result:"disable password div"});
 			chrome.browserAction.setIcon({path:"icons/Extrasafe_red19.png"});
 			chrome.browserAction.setTitle({title: "Click to enable extrasafe"});
 		}
 		else{
-			chrome.tabs.sendMessage(currentTab.id,{result:"enable password div"});
+			broadcast({result:"enable password div"});
 			chrome.browserAction.setIcon({path:"icons/Extrasafe19.png"});
 			chrome.browserAction.setTitle({title: "Click to disable extrasafe"});
 		}
 	});
 });
+
+function broadcast(message){
+	chrome.tabs.query({}, function(tabs){
+		for (var i = 0; i < tabs.length; i++) {
+			chrome.tabs.sendMessage(tabs[i].id,message);
+		}
+	});
+}
+
+function findSiteTag(url){
+	console.error(siteUrl+" : "+url+" : "+siteTag);
+	if(siteTag != "" && siteUrl == url){
+		return;
+	}
+	siteUrl = url;
+	siteTag = "";
+	//Split the url with ".".
+	var siteUrlBreakupArray = url.split(".");
+
+	if(siteUrlBreakupArray.length == 0){
+		siteTag = siteUrlBreakupArray[0];
+	}
+
+	//check the array strings with the pattern.
+	for(var i=0; i<siteUrlBreakupArray.length; i++){
+		var tempVar = siteUrlBreakupArray[i];
+		if(tempVar.match("^com") || tempVar.match("^org") || tempVar.match("^gov")){
+			siteTag = siteUrlBreakupArray[i-1];
+			break;
+		}
+		else if(tempVar.match("^co")){
+			siteTag = siteUrlBreakupArray[i-1];
+			break;
+		}
+		else if(tempVar.match("^in")){
+			siteTag = siteUrlBreakupArray[i-1];
+			break;
+		}
+	}
+
+	//Remove the http:// tag if its in siteTag there.
+	if(siteTag.indexOf("://") > 0){
+		siteTag = siteTag.substr( siteTag.indexOf("://")+3, siteTag.length );
+	}
+	return;
+}
--- safari/safari.safariextension/src/bg/background.js
+++ safari/safari.safariextension/src/bg/background.js
@@ -1,8 +1,7 @@
 //Global variable site tag (eg: google).
 var siteTag = "";
-
-//Global variable tab (current tab).
-var currentTab = undefined;
+//Global variable for web page url. Useful for performance in findSiteTag.
+var siteUrl = "";
 
 /*//For web pages containing login form dynamically generated through ajax.
 //This function listens to xmlhttprequests and reruns the DOM modification script in content script.
@@ -20,59 +19,79 @@ chrome.webRequest.onCompleted.addListener(function(info){
 //Returns the password from algorithm to content script.
 safari.application.activeBrowserWindow.addEventListener("message", function(event){
 	if(event.name == "key up"){
-		safari.application.activeBrowserWindow.activeTab.page.dispatchMessage("result", { result: Hasher.passy(event.message.masterPassword,siteTag), fromInputField: event.message.fromInputField });
+		findSiteTag(event.target.url);
+		event.target.page.dispatchMessage("result", { result: Hasher.passy(event.message.masterPassword,siteTag), fromInputField: event.message.fromInputField });
 	}
-}, true);
-
-//For every update in tab (eg: opening google) this will capture the siteTag.
-//New tab is set as global variable.
-//TODO: some best approach if possible.
-safari.application.addEventListener("navigate", function(event) {
-		siteTag = "";
-
-		currentTab = safari.application.activeBrowserWindow.activeTab;
-		//Split the url with ".".
-		var siteUrlBreakupArray = currentTab.url.split(".");
-		if(siteUrlBreakupArray.length == 0){
-			siteTag = siteUrlBreakupArray[0];
-		}
-
-		//check the array strings with the pattern.
-		for(var i=0; i<siteUrlBreakupArray.length; i++){
-			var tempVar = siteUrlBreakupArray[i];
-			if(tempVar.match("^com") || tempVar.match("^org") || tempVar.match("^gov")){
-				siteTag = siteUrlBreakupArray[i-1];
-				break;
-			}
-			else if(tempVar.match("^co")){
-				siteTag = siteUrlBreakupArray[i-1];
-				break;
-			}
-			else if(tempVar.match("^in")){
-				siteTag = siteUrlBreakupArray[i-1];
-				break;
-			}
-		}
-
-		//Remove the http:// tag if its in siteTag there.
-		if(siteTag.indexOf("://") > 0){
-			siteTag = siteTag.substr( siteTag.indexOf("://")+3, siteTag.length );
-		}
-		window.alert(siteTag);
 }, true);
 
 safari.application.addEventListener("command", function(event){
 	if(event.command == "disable"){
-		safari.application.activeBrowserWindow.activeTab.page.dispatchMessage("disable password div", "");
-		safari.extension.toolbarItems[0].command = "enable";
-		safari.extension.toolbarItems[0].toolTip = "Click to enable extrasafe in this site";
-		safari.extension.toolbarItems[0].image = safari.extension.baseURI + "icons/Extrasafe_striked16.png";
+		broadcast("disable password div", "");
+		var image = safari.extension.baseURI + "icons/Extrasafe_striked16.png";
+		var windows = safari.extension.toolbarItems.length;
+		for(var i=0; i<windows; i++){
+			safari.extension.toolbarItems[i].command = "enable";
+			safari.extension.toolbarItems[i].toolTip = "Click to enable extrasafe in this site";
+			safari.extension.toolbarItems[i].image = image;
+		}
 	}
 	else if(event.command == "enable"){
-		safari.application.activeBrowserWindow.activeTab.page.dispatchMessage("enable password div", "");
-		safari.extension.toolbarItems[0].command = "disable";
-		safari.extension.toolbarItems[0].toolTip = "Click to disable extrasafe in this site";
-		safari.extension.toolbarItems[0].image = safari.extension.baseURI + "icons/Extrasafe16.png";
+		broadcast("enable password div", "");
+		var image = safari.extension.baseURI + "icons/Extrasafe16.png";
+		var windows = safari.extension.toolbarItems.length;
+		for(var i=0; i<windows; i++){
+			safari.extension.toolbarItems[0].command = "disable";
+			safari.extension.toolbarItems[0].toolTip = "Click to disable extrasafe in this site";
+			safari.extension.toolbarItems[i].image = image;
+		}
 	}
 	
 }, true);
+
+function broadcast(command,message){
+	var windows = safari.application.browserWindows;
+	for (var i = 0; i < windows.length; i++) {
+		var tabs = windows[i].tabs;
+		for (var j = 0; j < tabs.length; j++) {
+			tabs[j].page.dispatchMessage(command,message);
+		}
+	}
+}
+
+function findSiteTag(url){
+	console.error(siteUrl+" : "+url+" : "+siteTag);
+	if(siteTag != "" && siteUrl == url){
+		return;
+	}
+	siteUrl = url;
+	siteTag = "";
+	//Split the url with ".".
+	var siteUrlBreakupArray = url.split(".");
+
+	if(siteUrlBreakupArray.length == 0){
+		siteTag = siteUrlBreakupArray[0];
+	}
+
+	//check the array strings with the pattern.
+	for(var i=0; i<siteUrlBreakupArray.length; i++){
+		var tempVar = siteUrlBreakupArray[i];
+		if(tempVar.match("^com") || tempVar.match("^org") || tempVar.match("^gov")){
+			siteTag = siteUrlBreakupArray[i-1];
+			break;
+		}
+		else if(tempVar.match("^co")){
+			siteTag = siteUrlBreakupArray[i-1];
+			break;
+		}
+		else if(tempVar.match("^in")){
+			siteTag = siteUrlBreakupArray[i-1];
+			break;
+		}
+	}
+
+	//Remove the http:// tag if its in siteTag there.
+	if(siteTag.indexOf("://") > 0){
+		siteTag = siteTag.substr( siteTag.indexOf("://")+3, siteTag.length );
+	}
+	return;
+}
