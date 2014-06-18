ff48052adc2f838b53219fd648838df0c6e3e774
Sai Pranav <rsaipranav92@gmail.com>
2014-06-19 03:16:27 +0530

    modified hasher a lot to make tests pass. Now very very minimum tests fails 3/30004

--- tests/hasher.js
+++ tests/hasher.js
@@ -6,10 +6,14 @@ Hasher = {
 	password: "", //output password
 	start: 0, //start from triming
 	end: 10, //end for triming
-	specialCharacter : new Array("@","!","&","*","#"),
-	injectCharacter : new Array("a","b","c","d","e","F","G","H","I","J"),
-	noPos : new Array(),
-	charPos : new Array(),
+	specialCharacters : new Array("@","!","&","*","#","(",")","/"),
+	capitalCharacters : new Array("A","B","C","D","E","F","G","H"),
+	smallCharacters : new Array("a","b","c","d","e","f","g","h"),
+	numbers : new Array("0","1","2","3","4","5","6","7","8","9"),
+	specialCharactersPos : new Array(),
+	numberPos : new Array(),
+	smallCharactersPos : new Array(),
+	capitalCharactersPos : new Array(),
 	iterator : 0,
 	
 	//Call the crypto graphic algorithm.
@@ -25,7 +29,7 @@ Hasher = {
 	
 	//Extrasafe modification
 	extrasafeModification: function(){
-					Hasher.countNoAndChars();
+					/*Hasher.countNoAndChars();
 
 					//all are numbers
 					if(Hasher.charPos.length == 0){
@@ -37,12 +41,67 @@ Hasher = {
 					else if(Hasher.noPos.length == 0){
 						Hasher.password = Hasher.replaceAt(0, '0');
 						Hasher.password = Hasher.replaceAt(3, '3');
-						Hasher.password = Hasher.replaceAt(9, '9');
+						Hasher.password = Hasher.replaceAt(6, '6');
 					}
 
 					Hasher.countNoAndChars();
-					Hasher.addSpecialCharacters();
+					Hasher.addSpecialCharacters();*/
+					Hasher.fillCountCharacters();
+					Hasher.checker();
 				},
+
+	checker: function(){
+					//all small characters
+					if(/^[a-z]*$/.test(Hasher.password) == true || Hasher.numberPos.length < 4){
+						var replaceIndex = Hasher.getReplaceIndex(Hasher.smallCharactersPos, 0);
+						Hasher.password = Hasher.replaceAt(replaceIndex, Hasher.numbers[((Hasher.password[Hasher.smallCharactersPos[Hasher.smallCharactersPos.length-1]]).charCodeAt(0))%(Hasher.numbers.length-1)]);
+						Hasher.smallCharactersPos.push(replaceIndex);
+						replaceIndex = Hasher.getReplaceIndex(Hasher.smallCharactersPos, 1);
+						Hasher.password = Hasher.replaceAt(replaceIndex, Hasher.numbers[((Hasher.password[Hasher.smallCharactersPos[Hasher.smallCharactersPos.length-1]]).charCodeAt(0))%(Hasher.numbers.length-1)]);
+						Hasher.smallCharactersPos.push(replaceIndex);
+						replaceIndex = Hasher.getReplaceIndex(Hasher.smallCharactersPos, 2);
+						Hasher.password = Hasher.replaceAt(replaceIndex, Hasher.numbers[((Hasher.password[Hasher.smallCharactersPos[Hasher.smallCharactersPos.length-1]]).charCodeAt(0))%(Hasher.numbers.length-1)]);
+						Hasher.smallCharactersPos.push(replaceIndex);
+					}
+					//all numbers
+					if(/^[0-9]*$/.test(Hasher.password) == true || Hasher.smallCharactersPos.length < 4){
+						var replaceIndex = Hasher.getReplaceIndex(Hasher.numberPos, 0);
+						Hasher.password = Hasher.replaceAt(replaceIndex, Hasher.smallCharacters[((Hasher.password[Hasher.numberPos[Hasher.numberPos.length-1]]).charCodeAt(0))%(Hasher.smallCharacters.length-1)]);
+						Hasher.smallCharactersPos.push(replaceIndex);
+						replaceIndex = Hasher.getReplaceIndex(Hasher.numberPos, 1);
+						Hasher.password = Hasher.replaceAt(replaceIndex, Hasher.smallCharacters[((Hasher.password[Hasher.numberPos[Hasher.numberPos.length-1]]).charCodeAt(0))%(Hasher.smallCharacters.length-1)]);
+						Hasher.smallCharactersPos.push(replaceIndex);
+						replaceIndex = Hasher.getReplaceIndex(Hasher.numberPos, 2);
+						Hasher.password = Hasher.replaceAt(replaceIndex, Hasher.smallCharacters[((Hasher.password[Hasher.numberPos[Hasher.numberPos.length-1]]).charCodeAt(0))%(Hasher.smallCharacters.length-1)]);
+						Hasher.smallCharactersPos.push(replaceIndex);
+					}
+					//capital characters not found
+					if(/^([a-z]|(@|!|&|#|\*)|[0-9])*$/.test(Hasher.password) == true){
+						var replaceIndex = Hasher.getReplaceIndex(Hasher.smallCharactersPos, 0);
+						Hasher.password = Hasher.replaceAt(replaceIndex, Hasher.capitalCharacters[((Hasher.password[Hasher.smallCharactersPos[Hasher.smallCharactersPos.length-1]]).charCodeAt(0))%(Hasher.capitalCharacters.length-1)]);
+						replaceIndex = Hasher.getReplaceIndex(Hasher.smallCharactersPos, 1);
+						Hasher.password = Hasher.replaceAt(replaceIndex, Hasher.capitalCharacters[((Hasher.password[Hasher.smallCharactersPos[Hasher.smallCharactersPos.length-1]]).charCodeAt(0))%(Hasher.capitalCharacters.length-1)]);
+						//replaceIndex = Hasher.getReplaceIndex(Hasher.smallCharactersPos, 2);
+						//Hasher.password = Hasher.replaceAt(replaceIndex, Hasher.capitalCharacters[((Hasher.password[Hasher.smallCharactersPos[Hasher.smallCharactersPos.length-1]]).charCodeAt(0))%(Hasher.capitalCharacters.length-1)]);
+					}
+					//special characters not found
+					if(/^([a-z]|[A-Z]|[0-9])*$/.test(Hasher.password) == true){
+						var replaceIndex = Hasher.getReplaceIndex(Hasher.numberPos, 0);
+						Hasher.password = Hasher.replaceAt(replaceIndex, Hasher.specialCharacters[((Hasher.password[Hasher.numberPos[Hasher.numberPos.length-1]]).charCodeAt(0))%(Hasher.specialCharacters.length-1)]);
+						//replaceIndex = Hasher.getReplaceIndex(Hasher.numberPos, 1);
+						//Hasher.password = Hasher.replaceAt(replaceIndex, Hasher.specialCharacters[((Hasher.password[Hasher.numberPos[Hasher.numberPos.length-1]]).charCodeAt(0))%(Hasher.specialCharacters.length-1)]);
+					}
+				},
+
+	getReplaceIndex: function(array, charAt){
+					var index = array[(Hasher.password.charCodeAt(charAt))%(Math.ceil(array.length/2))];
+					if(index == 0){
+						Hasher.password = "."+Hasher.password;
+						index = 1;
+					}
+					index = index - 1;
+					return index;
+				},			
 	
 	addSpecialCharacters: function(){
 					//Tweaking the password to include special characters (at the position of the middle digit)
@@ -54,7 +113,7 @@ Hasher = {
 
 	//Utility Helper methods - replaceAt(index to be replaced, character to be placed)
 	replaceAt: function(index, character) {
-      						return Hasher.password.substr(0, index) + character + Hasher.password.substr(index+character.length);
+   							return Hasher.password.substr(0, index) + character + Hasher.password.substr(index+character.length);
 						},
 
 	//Utility Helper methods - replaceAt(index to be replaced, character to be placed)
@@ -69,6 +128,24 @@ Hasher = {
 						}
 					}	
 				},
+
+	fillCountCharacters: function() {
+					Hasher.specialCharactersPos.length = Hasher.numberPos.length = Hasher.smallCharactersPos.length = Hasher.capitalCharactersPos.length = 0;
+					for(var iterator=0;iterator<Hasher.password.length;iterator++) {
+						if(/^[a-z]$/.test(Hasher.password[iterator])){
+							Hasher.smallCharactersPos[Hasher.smallCharactersPos.length++]=iterator;
+						}
+						else if(/^[0-9]$/.test(Hasher.password[iterator])){
+							Hasher.numberPos[Hasher.numberPos.length++]=iterator;
+						}
+						else if(/^[A-Z]$/.test(Hasher.password[iterator])){
+							Hasher.capitalCharactersPos[Hasher.capitalCharactersPos.length++]=iterator;
+						}
+						else if(/^\W$/.test(Hasher.password[iterator])){
+							Hasher.capitalCharactersPos[Hasher.capitalCharactersPos.length++]=iterator;
+						}
+					}	
+				},			
 
 	
 	//Single method to call from outside to return the hashed and modified password.
--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,5 @@
 test( 'Hasher.passy("","")', function() {
-	var testRuns = 10;
+	var testRuns = 10000;
 	var password = Hasher.passy("","");
 	for(var i=0; i < testRuns; i++){
 		equal( Hasher.passy("",""), password, "Passed!" );
@@ -7,32 +7,39 @@ test( 'Hasher.passy("","")', function() {
 });
 
 test( 'Hasher.passy("e@1#$fpQc%**(","Extrasafe")', function() {
-	equal( Hasher.passy("e@1#$fpQc%**(","Extrasafe"), "4664#63E79", "Passed!" );
+	changeHasherOptions("", 0, 10);
+	equal( Hasher.passy("e@1#$fpQc%**(","Extrasafe"), "4!b486Ae79", "Passed!" );
 });
 
+/*test( 'Summa', function() {
+	changeHasherOptions("", 0, 15);
+	equal( Hasher.passy("Extrasafe1681","1681"), "&3feeeeeff", "Passed!" );
+	changeHasherOptions("", 0, 10);
+});*/
+
 test( 'Checking with option : Extra Security Sequence', function() {
-	var testRuns = 100;
+	var testRuns = 1;
 	changeHasherOptions("A#1-@26Z", 0, 10);
 	for(var i=0; i < testRuns; i++){
-		equal( Hasher.passy("e@1#$fpQc%**(","Extrasafe"), "ebb37Cd#c9", "Passed!" );
+		equal( Hasher.passy("e@1#$fpQc%**(","Extrasafe"), "Fe!b37cd6c9", "Passed!" );
 	}
 	changeHasherOptions("", 0, 10);
 });
 
 test( 'Checking with option : Start Index', function() {
-	var testRuns = 10;
+	var testRuns = 1;
 	changeHasherOptions("", 2, 10);
 	for(var i=0; i < testRuns; i++){
-		equal( Hasher.passy("Extrasafe","e@1#$fpQc%**("), "8d61D*68", "Passed!" );
+		equal( Hasher.passy("Extrasafe","e@1#$fpQc%**("), "8B@1d168", "Passed!" );
 	}
 	changeHasherOptions("", 0, 10);
 });
 
 test( 'Checking with option : End Index', function() {
-	var testRuns = 10;
+	var testRuns = 1;
 	changeHasherOptions("", 0, 128);
 	for(var i=0; i < testRuns; i++){
-		equal( Hasher.passy("Extrasafe","e@1#$fpQc%**("), "db8d61d168cd621cb2a17079cf1701de38b3d6b474efc43467019841301ceedbE@b685140400b6c3aa1b034212a3eb83e162eede758de00b4bc139ce6cc51565", "Passed!" );
+		equal( Hasher.passy("Extrasafe","e@1#$fpQc%**("), "dbBd61d16Bcd621cb2a17079cf1701de#8b3d6b474efc43467019841301ceedbe0b685140400b6c3aa1b034212a3eb83e162eede758de00b4bc139ce6cc51565", "Extrasafe"+" :: "+"e@1#$fpQc%**(" );
 	}
 	changeHasherOptions("", 0, 10);
 });
@@ -46,37 +53,37 @@ test( 'Checking with option : End Index', function() {
 });*/
 
 test( 'Check for Special Characters', function() {
-	var testRuns = 1;
+	var testRuns = 5000;
 	changeHasherOptions("", 0, 15);
 	for(var i=0; i < testRuns; i++){
-		equal( checkForSpecialChars( Hasher.passy("Extrasafe"+i,""+i) ), true, "Passed!" );
+		equal( checkForSpecialChars( Hasher.passy("Extrasafe"+i,""+i) ), true, Hasher.passy("Extrasafe"+i,""+i) );
 	}
 	changeHasherOptions("", 0, 10);
 });
 
 test( 'Check for Upper Case', function() {
-	var testRuns = 1;
+	var testRuns = 5000;
 	changeHasherOptions("", 0, 15);
 	for(var i=0; i < testRuns; i++){
-		equal( checkForUpperCase( Hasher.passy("Extrasafe"+i,""+i) ), true, "Passed!" );
+		equal( checkForUpperCase( Hasher.passy("Extrasafe"+i,""+i) ), true, Hasher.passy("Extrasafe"+i,""+i) );
 	}
 	changeHasherOptions("", 0, 10);
 });
 
 test( 'Check for Lower Case', function() {
-	var testRuns = 20000;
+	var testRuns = 5000;
 	changeHasherOptions("", 0, 15);
 	for(var i=0; i < testRuns; i++){
-		equal( checkForLowerCase( Hasher.passy("Extrasafe"+i,""+i) ), true, "Passed!" );
+		equal( checkForLowerCase( Hasher.passy("Extrasafe"+i,""+i) ), true, Hasher.passy("Extrasafe"+i,""+i) );
 	}
 	changeHasherOptions("", 0, 10);
 });
 
 test( 'Check for Numbers', function() {
-	var testRuns = 20000;
+	var testRuns = 5000;
 	changeHasherOptions("", 0, 15);
 	for(var i=0; i < testRuns; i++){
-		equal( checkForNumbers( Hasher.passy("Extrasafe"+i,""+i) ), true, "Passed!" );
+		equal( checkForNumbers( Hasher.passy("Extrasafe"+i,""+i) ), true, Hasher.passy("Extrasafe"+i,""+i) );
 	}
 	changeHasherOptions("", 0, 10);
 });
