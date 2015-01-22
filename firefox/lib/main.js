bac68e7e52a8301e801ffdd75f3d16e39bfb1bc5
Sai Pranav <rsaipranav92@gmail.com>
2015-01-22 16:45:10 +0530

    added another check in site tag extraction

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -142,7 +142,8 @@ function findSiteTag(url){
 	//Split the url with ".".
 	var siteUrlBreakupArray = url.split(".");
 
-	if(siteUrlBreakupArray.length == 0){
+	//if the url has single word then return that as site name
+	if(siteUrlBreakupArray.length == 1){
 		siteTag = siteUrlBreakupArray[0];
 		return;
 	}
@@ -150,9 +151,11 @@ function findSiteTag(url){
 	//check the array strings with the pattern.
 	for(var i=(siteUrlBreakupArray.length-1); i>=0; i--){
 		var tempVar = siteUrlBreakupArray[i];
-		if(tempVar.match("^(com|org|net|int|edu|gov|mil|co)($|[^a-zA-Z0-9])") || tempVar.match("^(in|ae|an|aq|ar|au|bd|be|bm|br|bw|ca|ch|cl|cn|cr|cs|cu|cz|de|dk|eg|es|eu|fi|fr|gb|ge|gl|gr|hk|hu|id|ie|il|iq|ir|is|it|jm|jp|ke|kh|kw|lk|mm|mn|mx|my|ng|np|nz|om|pe|pt|qa|ro|ru|sa|se|sg|su|th|uk|us|uz|va|ve|vn|za|zw)($|[^a-zA-Z0-9])")){
-			siteTag = siteUrlBreakupArray[i-1];
-			break;
+		if(tempVar.match("^(com|org|net|int|edu|gov|mil)($|[^a-zA-Z0-9])") || tempVar.match("^(in|ae|an|aq|ar|au|bd|be|bm|br|bw|ca|ch|cl|cn|co|cr|cs|cu|cz|de|dk|eg|es|eu|fi|fr|gb|ge|gl|gr|hk|hu|id|ie|il|iq|ir|is|it|jm|jp|ke|kh|kw|lk|mm|mn|mx|my|ng|np|nz|om|pe|pt|qa|ro|ru|sa|se|sg|su|th|uk|us|uz|va|ve|vn|za|zw)($|[^a-zA-Z0-9])")){
+			if( ! (tempVar.match("^(com|org|net|int|edu|gov|mil)($|[^a-zA-Z0-9])") || tempVar.match("^(in|ae|an|aq|ar|au|bd|be|bm|br|bw|ca|ch|cl|cn|co|cr|cs|cu|cz|de|dk|eg|es|eu|fi|fr|gb|ge|gl|gr|hk|hu|id|ie|il|iq|ir|is|it|jm|jp|ke|kh|kw|lk|mm|mn|mx|my|ng|np|nz|om|pe|pt|qa|ro|ru|sa|se|sg|su|th|uk|us|uz|va|ve|vn|za|zw)($|[^a-zA-Z0-9])") ) ){
+				siteTag = siteUrlBreakupArray[i-1];
+				break;
+			}
 		}
 	}
 	
