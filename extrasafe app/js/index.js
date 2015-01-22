79d290d72ef62c123c0f842035a9721da12d8829
Sai Pranav <rsaipranav92@gmail.com>
2015-01-22 16:34:06 +0530

    added seperate htmls for every page, added url grabber instead of site name

--- extrasafe app/js/index.js
+++ extrasafe app/js/index.js
@@ -1,14 +1,11 @@
 var store = window.localStorage;
+var siteTag = "";
 
 var app = {
 	initialize: function() {
-        this.bindEvents();
-    },
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        this.onDeviceReady();
     },
     onDeviceReady: function() {
-        console.log("device ready!");
         if(store.getItem("extraSecuritySequence") == null){
         	store.setItem("extraSecuritySequence", "");
         }
@@ -19,133 +16,57 @@ var app = {
         	store.setItem("endIndex", 12);
         }
         if(store.getItem("sites") == null){
-        	store.setItem("sites", JSON.stringify(['google', 'facebook']));
+        	store.setItem("sites", JSON.stringify([]));
         }
-        $("#extra-security-sequence").val(store.getItem("extraSecuritySequence"));
-		$("#start-index").val(store.getItem("startIndex"));
-		$("#end-index").val(store.getItem("endIndex"));
     }
 };
-
-$("#options-page").hide();
-$("#about-page").hide();
-
-var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substrRegex;
- 
-    // an array that will be populated with substring matches
-    matches = [];
- 
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
- 
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        // the typeahead jQuery plugin expects suggestions to a
-        // JavaScript object, refer to typeahead docs for more info
-        matches.push({ value: str });
-      }
-    });
- 
-    cb(matches);
-  };
-};
-
-$('#site-name').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 2
-},
-{
-  name: 'sites',
-  displayKey: 'value',
-  source: substringMatcher(JSON.parse(store.getItem("sites")))
-}
-);
 
 $("#site-password").click(function(){
 	$("#site-password").select();
 });
 
-$("#site-name").keyup(function(){
-	if($("#site-name").val().match(/[^a-z0-9]/g)){
-		$("#site-name").parent().addClass("has-error");
-		$("#site-name").parent().parent().siblings(".error1").show();
+$("#site-url").focusout(function(){
+	var siteUrl = $("#site-url");
+	siteUrl.parent().removeClass("has-error");
+	siteUrl.parent().siblings(".errors").html("");
+	siteUrl.parent().siblings(".errors").hide();
+	findSiteTag(siteUrl.val());
+	siteUrl.val(siteTag);
+});
+
+$("#master-password").keyup(function(e){
+	var masterPassword = $("#master-password");
+	masterPassword.parent().removeClass("has-error");
+	masterPassword.siblings(".errors").html("");
+	masterPassword.siblings(".errors").hide();
+	if(masterPassword.val().match(/\s/g)){
+		masterPassword.parent().addClass("has-error");
+		masterPassword.siblings(".errors").append("<div>Space is not allowed</div>");
+		masterPassword.siblings(".errors").show();
 	}
-	else{
-		$("#site-name").parent().removeClass("has-error");
-		$("#site-name").parent().parent().siblings(".error1").hide();
-	}
-});
-
-$("#options-button").click(function(){
-	$("#home-page").hide();
-	$("#about-page").hide();
-	$("#options-page").show();
-});
-
-$("#about-button").click(function(){
-	$("#home-page").hide();
-	$("#options-page").hide();
-	$("#about-page").show();
-});
-
-$(".back-button").click(function(){
-	$("#options-page").hide();
-	$("#about-page").hide();
-	$("#home-page").show();
-});
-
-$("#start-index").focusout(function(){
-	if($("#start-index").val() > 115 || $("#start-index").val() < 0){
-		$("#start-index").parent().addClass("has-error");
-		$("#start-index").siblings(".error").show();
-		$("#start-index").keyup(function(){
-			$("#start-index").parent().removeClass("has-error");
-			$("#start-index").siblings(".error").hide();
-		});
-	}
-});
-
-$("#end-index").focusout(function(){
-	if($("#end-index").val() > 128 || $("#end-index").val() < 12){
-		$("#end-index").parent().addClass("has-error");
-		$("#end-index").siblings(".error").show();
-		$("#end-index").keyup(function(){
-			$("#end-index").parent().removeClass("has-error");
-			$("#end-index").siblings(".error").hide();
-		});
-	}
-});
-
-$("#about-button").click(function(){
-	$("#home-page").hide();
-	$("#options-page").hide();
-	$("#about-page").show();
 });
 
 $("#generate-button").click(function(){
 
 	var goAhead = false;
 	//get all required parameters
-	var masterPassword = $("#master-password").val();
-	var siteName = $("#site-name").val();
+	var masterPassword = $("#master-password");
+	var siteUrl = $("#site-url");
 
 	//check master password
-	if(masterPassword == ""){
-		$("#master-password").parent().addClass("has-error");
-		$("#master-password").siblings(".error").show();
-		$("#master-password").keyup(function(){
-			$("#master-password").parent().removeClass("has-error");
-			$("#master-password").siblings(".error").hide();
-		});
-		$("#master-password").focus();
+	masterPassword.siblings(".errors").html("");
+	if(masterPassword.val() == ""){
+		masterPassword.parent().addClass("has-error");
+		masterPassword.siblings(".errors").append("<div>Mandatory Field</div>");
+		masterPassword.siblings(".errors").show();
+		masterPassword.focus();
+	}
+	else if(masterPassword.val().match(/\s/g)){
+		masterPassword.parent().addClass("has-error");
+		masterPassword.siblings(".errors").append("<div>Space is not allowed</div>");
+		masterPassword.siblings(".errors").show();
 	}
 	else{
-		$("#master-password").parent().removeClass("has-error");
 		goAhead = true;
 	}
 
@@ -167,38 +88,33 @@ $("#generate-button").click(function(){
 
 	if(goAhead){
 		//check site name
-		if(siteName == ""){
-			$("#site-name").parent().addClass("has-error");
-			$("#site-name").parent().parent().siblings(".error2").show();
-			$("#site-name").keyup(function(){
-				$("#site-name").parent().removeClass("has-error");
-				$("#site-name").parent().parent().siblings(".error2").hide();
-			});
-			$("#site-name").focus();
+		siteUrl.parent().siblings(".errors").html("");
+		if(siteUrl.val() == ""){
+			siteUrl.parent().addClass("has-error");
+			siteUrl.parent().siblings(".errors").append("<div>Mandatory Field</div>");
+			siteUrl.parent().siblings(".errors").show();
+			siteUrl.focus();
 			goAhead = false;
 		}
 		else{
-			$("#site-name").parent().removeClass("has-error");
+			siteUrl.parent().removeClass("has-error");
 			goAhead = true;
 		}
 	}
 
-	var extraSequence = store.getItem("extraSecuritySequence");
+	var extraSecuritySequence = store.getItem("extraSecuritySequence");
 	var startIndex = store.getItem("startIndex");
 	var endIndex = store.getItem("endIndex");			
 
 	if(goAhead){
-		Hasher.start = startIndex;
-		Hasher.end = endIndex;
-		Hasher.extraSecuritySequence = extraSequence;
-		$("#site-password").val( Hasher.passy($("#master-password").val(), $("#site-name").val()) );
+		$("#site-password").val( Hasher.passy(masterPassword.val(), siteUrl.val(), extraSecuritySequence, startIndex, endIndex));
 		$("#tooltip").html("Your site password is generated, please copy it from the Site Password box").fadeIn();
 		setTimeout(function(){
 			$("#tooltip").fadeOut();
 		},3000);
 		var sites = JSON.parse(store.getItem("sites"));
-		if($.inArray($("#site-name").val(),sites) == -1){
-			sites.push($("#site-name").val());
+		if($.inArray(siteUrl.val(),sites) == -1){
+			sites.push(siteUrl.val());
 		}
 		store.setItem("sites", JSON.stringify(sites));
 		return;
@@ -209,65 +125,50 @@ $("#generate-button").click(function(){
 
 });
 
-$("#save-button").click(function(){
-	var extraSequence = $("#extra-security-sequence").val();
-	var startIndex = parseInt($("#start-index").val());
-	var endIndex = parseInt($("#end-index").val());
-	if( (startIndex<0) || (endIndex>128) || (startIndex>=endIndex) || (startIndex>115) || (endIndex<12) || ((endIndex-startIndex)<12) ){
-		$("#tooltip").html("Failed to save your options, please rectify the errors in red input boxes").fadeIn();
-		setTimeout(function(){
-			$("#tooltip").fadeOut();
-		},3000);
-		return;
-	}
-	if( (isNaN(startIndex)) || (isNaN(endIndex))  ){
-		$("#tooltip").html("Failed to save your options, please enter your options").fadeIn();
-		setTimeout(function(){
-			$("#tooltip").fadeOut();
-		},3000);
-		return;
-	}
-
-	store.setItem("extraSecuritySequence", extraSequence);
-	store.setItem("startIndex", startIndex);
-	store.setItem("endIndex", endIndex);
-
-	$("#tooltip").html("All your options are saved").fadeIn();
-	setTimeout(function(){
-		$("#tooltip").fadeOut();
-	},2000);
-});
-
-$("#cancel-button").click(function(){
-	var extraSequence = store.getItem("extraSecuritySequence");
-	var startIndex = store.getItem("startIndex");
-	var endIndex = store.getItem("endIndex");
-	$("#extra-security-sequence").val(extraSequence);
-	$("#start-index").val(startIndex);
-	$("#end-index").val(endIndex);
-	$("#tooltip").html("Your operations are canceled").fadeIn();
-	setTimeout(function(){
-		$("#tooltip").fadeOut();
-	},2000);
-});
-
-$("#reset-button").click(function(){
-	var extraSequence = "";
-	var startIndex = 0;
-	var endIndex = 12;
-	store.setItem("extraSecuritySequence", extraSequence);
-	store.setItem("startIndex", startIndex);
-	store.setItem("endIndex", endIndex);
-	$("#extra-security-sequence").val(extraSequence);
-	$("#start-index").val(startIndex);
-	$("#end-index").val(endIndex);
-	$("#tooltip").html("All your options are reset to default").fadeIn();
-	setTimeout(function(){
-		$("#tooltip").fadeOut();
-	},2500);
-});
-
-$("#show-password").click(function(){
+$("#show-password").hover(function(){
 	var type = ( $("#master-password").attr("type") == "text" ) ? "password" : "text";
 	$("#master-password").attr("type",type);
 });
+
+$("#clear").click(function(){
+	store.clear();
+	$(this).attr("value","CLEARED");
+	$(this).css("background-color","#228B22");
+});
+
+function findSiteTag(url){
+	if(siteTag != "" && siteUrl == url){
+		return;
+	}
+	siteUrl = url;
+	siteTag = "";
+
+	//Remove the http:// tag if its in siteTag there.
+	if(url.indexOf("://") > 0){
+		url = url.substr( url.indexOf("://")+3, url.length );
+	}
+	//Split the url with "/"
+	var url = url.split("/")[0];
+
+	//Split the url with ".".
+	var siteUrlBreakupArray = url.split(".");
+
+	//if the url is single word then return that word as site name
+	if(siteUrlBreakupArray.length == 1){
+		siteTag = siteUrlBreakupArray[0];
+		return;
+	}
+
+	//check the array strings with the pattern.
+	for(var i=(siteUrlBreakupArray.length-1); i>=0; i--){
+		var tempVar = siteUrlBreakupArray[i];
+		if(tempVar.match("^(com|org|net|int|edu|gov|mil)($|[^a-zA-Z0-9])") || tempVar.match("^(in|ae|an|aq|ar|au|bd|be|bm|br|bw|ca|ch|cl|cn|co|cr|cs|cu|cz|de|dk|eg|es|eu|fi|fr|gb|ge|gl|gr|hk|hu|id|ie|il|iq|ir|is|it|jm|jp|ke|kh|kw|lk|mm|mn|mx|my|ng|np|nz|om|pe|pt|qa|ro|ru|sa|se|sg|su|th|uk|us|uz|va|ve|vn|za|zw)($|[^a-zA-Z0-9])")){
+			if( ! (siteUrlBreakupArray[i-1].match("^(com|org|net|int|edu|gov|mil|co)($|[^a-zA-Z0-9])") || siteUrlBreakupArray[i-1].match("^(in|ae|an|aq|ar|au|bd|be|bm|br|bw|ca|ch|cl|cn|co|cr|cs|cu|cz|de|dk|eg|es|eu|fi|fr|gb|ge|gl|gr|hk|hu|id|ie|il|iq|ir|is|it|jm|jp|ke|kh|kw|lk|mm|mn|mx|my|ng|np|nz|om|pe|pt|qa|ro|ru|sa|se|sg|su|th|uk|us|uz|va|ve|vn|za|zw)($|[^a-zA-Z0-9])") ) ){
+				siteTag = siteUrlBreakupArray[i-1];
+				break;
+			}
+		}
+	}
+	
+	return;
+}
